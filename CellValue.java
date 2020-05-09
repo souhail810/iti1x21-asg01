de11c3ce95fa629a67abc905e7bc0f834346a61b
Andrew Forward <aforward@gmail.com>
2020-05-09 15:35:52 -0400

    Assignment 01 template

---
+++ CellValue.java
@@ -0,0 +1,15 @@
+/**
+ * An enum class that defines the values
+ *  <b>INVALID</b>,
+ *  <b>EMPTY</b>,
+ *  <b>X</b>, and
+ *  <b>O</b>.
+ *
+ * @author Guy-Vincent Jourdan, University of Ottawa
+ */
+
+public enum CellValue {
+
+	// YOUR CODE HERE
+
+}
